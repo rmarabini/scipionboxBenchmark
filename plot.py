2a1dd9581d999a06366651854ef33315eb719237
rmarabini <roberto@cnb.csic.es>
2017-06-27 00:22:00 +0200

    first version plot any key between any two ids

--- plot.py
+++ plot.py
@@ -3,6 +3,7 @@ import glob
 import os
 import time
 import sys
+import sqlite3 as lite
 
 
 from optparse import OptionParser
@@ -10,59 +11,72 @@ from optparse import OptionParser
 parser = OptionParser()
 parser.add_option("-i", "--inputfile", dest="filename",
                   help="input  FILE")
+parser.add_option("-l", "--labels", dest="labels",
+                  help="labels to display")
+parser.add_option("-d", "--minId", dest="minId",
+                  help="start at this id", type="int", default=1)
+parser.add_option("-D", "--maxID", dest="maxId",
+                  help="end at this id", type="int", default=1000000)
 
 (options, args) = parser.parse_args()
-if options.filename:
-   print "filename", options.filename
 
-#acces popstcript
+tableName = "log"
+conn = lite.connect(options.filename, isolation_level=None)
+cur = conn.cursor()
+cur.execute("select julianday(timestamp)  from %s where id=%d" %
+                    (tableName,  options.minId))
+initTime = cur.fetchone()[0]
 
-exit(1)
+cur.execute("""select (julianday(timestamp) - %f)*24.*60.  
+               from %s 
+               where id>=%d and id <=%d""" % 
+               (initTime, tableName,  options.minId,  options.maxId ))
 
-d = collections.OrderedDict()
+timeValues = [r[0] for r in cur.fetchall()]
 
-searchedfile = glob.glob("GRID_0?/DATA/Images-Disc?/GridSquare_*/Data/*_frames*.mrc")
-#searchedfile = glob.glob("20160930_CSM_AdDELTA7/GRID_0?/DATA/Images-Disc?/GridSquare_*/Data/*_frames*.mrc")
-files = sorted( searchedfile, key = lambda file: os.path.getctime(file))
+#id INTEGER, 
+#timestamp DATE,
+#cpu FLOAT,
+#mem FLOAT,
+#swap FLOAT,
+#gpuMem_0 FLOAT,
+#gpuUse_0 FLOAT,
+#gpuTem_0 FLOAT,
+#eth1_send FLOAT,
+#eth1_recv FLOAT,
+#disk_read FLOAT,
+#disk_write FLOAT
 
-previousT = 0
-x=[]
-y=[]
-counter = 0
-for file in files:
-    T = os.path.getctime(file)
-    interval = T -  previousT
-    print file, interval
-    previousT = T
-    x.append(counter)
-    y.append(interval)
-    counter += 1
+data = {}
+def get(name):
+    try:
+        cur.execute("""select %s 
+                       from %s 
+                       where id>=%d and id <=%d""" % (name, tableName, options.minId,  options.maxId ))
+    except Exception as e:
+        print("ERROR readind data (plotter). I continue")
+    return [r[0] for r in cur.fetchall()]
+
+for key in options.labels.split():
+    data[key] = get(key)
+
 
 #print x, y
 
 
 import matplotlib.pyplot as plt
-import numpy as np
-import matplotlib.font_manager as font_manager
-
-#t = np.arange(0.0, 2.0, 0.01)
-#s = 1 + np.sin(2*np.pi*t)
 f = plt.figure()
-plt.semilogy(x, y, linewidth=2.5)
+for key in options.labels.split():
+    plt.plot(timeValues, data[key], linewidth=2.5, label=key)
 
 plt.xlabel('Movie #')
 plt.ylabel('Creation Time (sec)')
 plt.title('Movie Production Interval')
 plt.grid(True)
-plt.savefig("test.png")
-plt.savefig("test.pdf")
+#plt.legend(loc='best')
+plt.legend(loc=2).get_frame().set_alpha(0.5)
+##plt.savefig("test.png")
+##plt.savefig("test.pdf")
 plt.show()
 
-#histogram
-#n, bins, patches = plt.hist(y, 50, normed=1, facecolor='green', alpha=0.75)
-plt.hist(y, bins=5)
-plt.xlabel('Smarts')
-plt.ylabel('Probability')
-plt.title(r'$\mathrm{Histogram\ of\ IQ:}\ \mu=100,\ \sigma=15$')
-plt.show()
 
